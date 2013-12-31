7f734065157d54125b4ba279d5afa58d3055f7e3
Lujie Zhou <lujie.zhou@jago-ag.cn>
2013-12-31 11:29:21 +0800

    Signed-off-by: Lujie Zhou <lujie.zhou@jago-ag.cn>

--- themes/pidiao/views/layouts/main.php
+++ themes/pidiao/views/layouts/main.php
@@ -1,11 +1,12 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-    <meta http-equiv="content-language"  content="zh"/>
-    <meta http-equiv="Cache-Control" content="max-age=7200" />
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+    <meta http-equiv="content-language" content="zh"/>
+    <meta http-equiv="Cache-Control" content="max-age=7200"/>
     <meta content="IE=7" http-equiv="X-UA-Compatible"/>
-    <link type='text/css' rel='stylesheet' href='<?php echo Yii::app()->theme->baseUrl; ?>/css/common.css' />
+    <link type='text/css' rel='stylesheet' href='<?php echo Yii::app()->theme->baseUrl; ?>/css/common.css'/>
     <script type="text/javascript" src="<?php echo Yii::app()->theme->baseUrl; ?>/js/jquery-1.4.4.min.js"></script>
     <title><?php echo Yii::app()->params['title']; ?></title>
 <body>
@@ -24,20 +25,23 @@
             <span>您好，欢饮来到皮雕软包耗材批发商城！</span>
             <a href="" class="cor_red">登陆</a>
             <a href="" class="cor_red">注册享好礼</a>
+
             <div class="top_center">我的账户<i>arrow</i></div>
             <div class="top_daohang">网站导航<i>arrow</i></div>
         </div>
     </div>
 </div>
 <div class="head">
-    <div class="logo"><a href="<?php echo Yii::app()->baseUrl; ?>"><img alt="" src="<?php echo Yii::app()->theme->baseUrl; ?>/image/logo.png" width="227" height="80"></a></div>
+    <div class="logo"><a href="<?php echo Yii::app()->baseUrl; ?>"><img alt=""
+                                                                        src=""
+                                                                        width="227" height="80"></a></div>
     <div class="search">
         <div class="search_box">
             <input type="text" value=""/>
             <button></button>
         </div>
         <div class="search_hot">
-            热门搜索：<a href="">皮雕</a>  <a href="">软皮</a>  <a href="">压花</a>
+            热门搜索：<a href="">皮雕</a> <a href="">软皮</a> <a href="">压花</a>
         </div>
     </div>
     <div class="shopping_car">
@@ -46,11 +50,12 @@
 </div>
 <div class="nav">
     <ul class="nav_list">
-        <li class="current"><a href="<?php echo Yii::app()->baseUrl; ?>">首页</a></li>
-        <?php
+        <?php $class = isset(Yii::app()->params['categoryIds']) ? '' : 'current';
+        echo '<li class="' . $class . '"><a href="' . Yii::app()->baseUrl . '">首页</a></li>';
         $categories = Category::model()->findAllByAttributes(array('root' => '3', 'level' => 2));
-        foreach ($categories as $category) {
-            echo '<li><a href="'.Yii::app()->createUrl('catalog/index', array('key' => $category->category_id)).'">'.$category->name.'</a></li>';
+        foreach ($categories as $cate) {
+            $class = isset(Yii::app()->params['categoryIds']) && in_array($cate->category_id, Yii::app()->params['categoryIds']) ? 'current' : '';
+            echo '<li class="' . $class . '"><a href="' . Yii::app()->createUrl('catalog/index', array('key' => $cate->category_id)) . '">' . $cate->name . '</a></li>';
         }
         ?>
     </ul>
@@ -101,13 +106,17 @@
         </div>
         <div class="foot_call">
             <p class="font14 bold">咨询电话</p>
+
             <p class="font14 bold cor_r">13967414054</p>
         </div>
     </div>
     <div class="foot_u">
-        <p class="foot_link"><a href="">关于我们</a>|<a href="">联系我们</a>|<a href="">人才招聘</a>|<a href="">商家入驻</a>|<a href="">广告服务</a>|<a href="">手机商城</a>|<a href="">友情链接</a>|<a href="">销售联盟</a>|<a href="">皮雕社区</a>|<a href="">资源交流</a></p>
-        <p>Copyright ? 2013 - 2015 皮雕软包耗材批发商城 All Rights Reserved.   <a href="">站长统计</a></p>
-        <p>银河方舟  全程技术支持</p>
+        <p class="foot_link"><a href="">关于我们</a>|<a href="">联系我们</a>|<a href="">人才招聘</a>|<a href="">商家入驻</a>|<a href="">广告服务</a>|<a
+                href="">手机商城</a>|<a href="">友情链接</a>|<a href="">销售联盟</a>|<a href="">皮雕社区</a>|<a href="">资源交流</a></p>
+
+        <p>Copyright ? 2013 - 2015 皮雕软包耗材批发商城 All Rights Reserved. <a href="">站长统计</a></p>
+
+        <p>银河方舟 全程技术支持</p>
     </div>
 </div>
 </body>

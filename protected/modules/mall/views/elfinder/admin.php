b279cf2bed22ff7de96c7c11ce76457527600c6f
jeremy.zhou <gaolujie1989@126.com>
2014-11-10 09:50:18 +0800

    elfinder js conflict

--- protected/modules/mall/views/elfinder/admin.php
+++ protected/modules/mall/views/elfinder/admin.php
@@ -9,8 +9,9 @@ $this->menu = array();
 
 <?php
 // ElFinder widget
-$this->widget('ext.elFinder.ElFinderWidget', array(
-        'connectorRoute' => 'mall/elfinder/connector',
-    )
-);
+//$this->widget('ext.elFinder.ElFinderWidget', array(
+//        'connectorRoute' => 'mall/elfinder/connector',
+//    )
+//);
 ?>
+<iframe style="width: 100%; height: 405px" src="<?php echo Yii::app()->createUrl('/mall/elfinder/view') ?>"></iframe>
